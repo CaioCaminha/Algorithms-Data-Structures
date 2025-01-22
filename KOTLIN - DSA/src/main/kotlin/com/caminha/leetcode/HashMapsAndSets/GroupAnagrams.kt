c4da334fb73aba0d8d9feba6eef7c5a56d6a2232
Caio Caminha <caio.caminha@globalpay.com>
2025-01-22 06:25:45 -0300

    leetCode 49 Group Anagrams

--- KOTLIN - DSA/src/main/kotlin/com/caminha/leetcode/HashMapsAndSets/GroupAnagrams.kt
+++ KOTLIN - DSA/src/main/kotlin/com/caminha/leetcode/HashMapsAndSets/GroupAnagrams.kt
@@ -1,4 +1,73 @@
 package com.caminha.leetcode.HashMapsAndSets
 
-class GroupAnagrams {
+fun groupAnagrams(strs: Array<String>): List<List<String>> {
+    if(strs.size == 1) return listOf(strs.toList())
+
+    val sortedMap = mutableMapOf<String, Set<Int>>()
+    val result = mutableListOf<List<String>>()
+    strs.forEachIndexed { index, s ->
+        val sorted = s.toCharArray().sorted().joinToString("")
+        if(sortedMap.containsKey(sorted)) {
+            val currentIndexes = sortedMap[sorted]
+            sortedMap[sorted] = currentIndexes!! + setOf(index)
+        }else {
+            sortedMap.put(sorted, setOf(index))
+        }
+    }
+
+    sortedMap.values.forEach { value ->
+        result.add(
+            value.map {
+                strs[it]
+            }
+        )
+    }
+
+    return result
+}
+
+//fun compareStrings(list: List<String>): Boolean {
+//    val sortedMap = mutableMapOf<String, Set<Int>>()
+//    val result = mutableListOf<List<String>>()
+//    list.forEachIndexed { index, s ->
+//        val sorted = s.toCharArray().sorted().joinToString("")
+//        if(sortedMap.containsKey(sorted)) {
+//            val currentIndexes = sortedMap[sorted]
+//            sortedMap[sorted] = currentIndexes!! + setOf(index)
+//        }else {
+//            sortedMap.put(sorted, setOf(index))
+//        }
+//    }
+//
+////    sortedMap.entries.forEach { (key, value) ->
+////        result.add(
+////            value.map {
+////                list[it]
+////            }
+////        )
+////    }
+////
+//    sortedMap.values.forEach { value ->
+//        result.add(
+//            value.map {
+//                list[it]
+//            }
+//        )
+//    }
+//
+//    println(result)
+//
+//
+//    return true
+//}
+
+fun something(str: String) {
+    for(c in str.toCharArray()) {
+        println(c - 'a')
+    }
+}
+
+fun main() {
+    //println(groupAnagrams(arrayOf("eat","tea","tan","ate","nat","bat")))
+    println(something("eat"))
 }
--- KOTLIN - DSA/src/main/kotlin/com/caminha/leetcode/HashMapsAndSets/ValidSudoku.kt
+++ KOTLIN - DSA/src/main/kotlin/com/caminha/leetcode/HashMapsAndSets/ValidSudoku.kt
@@ -1,4 +1,27 @@
 package com.caminha.leetcode.HashMapsAndSets
 
-class ValidSudoku {
+fun isValidSudoku(board: Array<CharArray>): Boolean {
+    for (i in 0..2) {
+        for (j in 0..2) {
+            println(board[i][j])
+        }
+    }
+    return true
+}
+
+
+fun main() {
+    isValidSudoku(
+        arrayOf(
+            charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+            charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+            charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+            charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+            charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+            charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+            charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+            charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+            charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+        )
+    )
 }
